27d8402a5bd3d45ba1fc5ec62eb42f8f87e22e58
Zakarya-Netlinks <zakarya@netlinks.af>
2020-09-23 22:43:14 +0430

    Add other pages

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,27 @@
 $(function () {
+  let scroll = $(document).scrollTop();
+  let navHeight = $(".nav-area").outerHeight();
 
-    let scroll = $(document).scrollTop();
-    let navHeight = $(".nav-area").outerHeight();
-  
-    $(window).scroll(function () {
-      let scrolled = $(document).scrollTop();
-  
-      if (scrolled > navHeight) {
-        $(".nav-area").addClass("animate");
-      } else {
-        $(".nav-area").removeClass("animate");
-      }
-  
-      if (scrolled > scroll) {
-        $(".nav-area").removeClass("sticky");
-      } else {
-        $(".nav-area").addClass("sticky");
-      }
-  
-      scroll = $(document).scrollTop();
-    });
+  $(window).scroll(function () {
+    let scrolled = $(document).scrollTop();
+
+    if (scrolled > navHeight) {
+      $(".nav-area").addClass("animate");
+    } else {
+      $(".nav-area").removeClass("animate");
+    }
+
+    if (scrolled > scroll) {
+      $(".nav-area").removeClass("sticky");
+    } else {
+      $(".nav-area").addClass("sticky");
+    }
+
+    scroll = $(document).scrollTop();
   });
-  
-  let scroll = window.scrollY
-  
+});
+
+AOS.init({
+  duration: 10,
+  easing: "ease-in"
+})
