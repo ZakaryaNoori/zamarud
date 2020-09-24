89d4535ce8907481aa12f75f8f02da54be152a41
Zakarya-Netlinks <zakarya@netlinks.af>
2020-09-24 20:32:02 +0430

    Add new files

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,7 +21,39 @@ $(function () {
   });
 });
 
-AOS.init({
-  duration: 10,
-  easing: "ease-in"
-})
+
+$(function () { // wait for document ready
+  // init controller
+  var controller = new ScrollMagic.Controller();
+
+  // show pin state
+  function updateBox (e) {
+    if (e.type == "enter") {
+      $("#pin p").text("Pinned.");
+    } else {
+      $("#pin p").text("Unpinned.");
+    }
+  }
+
+  // build scenes
+  new ScrollMagic.Scene({triggerElement: "#trigger", duration: 150})
+    .setPin("#pin")
+    .setClassToggle("#pin", "green")
+    .on("enter leave", updateBox)
+    .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+
+  new ScrollMagic.Scene({triggerElement: "#trigger", duration: 150, offset: 300})
+    .setPin("#pin")
+    .setClassToggle("#pin", "green")
+    .on("enter leave", updateBox)
+    .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+
+  new ScrollMagic.Scene({triggerElement: "#trigger", duration: 150, offset: 600})
+    .setPin("#pin")
+    .setClassToggle("#pin", "green")
+    .on("enter leave", updateBox)
+    .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+});
